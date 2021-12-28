e98fbb8b42228f95f15e70a8c38b3bc827be68cf
Wien Dewani <wiennuruldewani@gmail.com>
2021-12-28 18:58:58 +0700

    init

---
+++ test.py
@@ -0,0 +1,5 @@
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications import MobileNetV2 
+
+load_model("D:\KULIAH\SEMESTER 7\Pembelajaran Mesin\Praktikum_ML\breastcancer-image\static\MLModule\model_resnet.tflite")
+
